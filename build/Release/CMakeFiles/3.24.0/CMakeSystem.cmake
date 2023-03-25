d9a81c3fb2d00d5b0d79d4993a81db36f8481f4f
Mateusz Karpiczenko <m.karpiczenko@mmb-drives.com.pl>
2023-03-25 12:53:43 +0100

    repo init

---
+++ build/Release/CMakeFiles/3.24.0/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-6.1.7601")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "6.1.7601")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("E:/MKA/projekty/cmake_Example_H743/cmake/gcc-arm-none-eabi.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
